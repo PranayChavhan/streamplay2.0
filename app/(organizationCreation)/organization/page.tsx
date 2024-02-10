2acf4bf14ffdce74f987382ec83d46eb03c50be4
Ayush-Bulbule <ayushbulbule24@gmail.com>
2024-02-11 04:02:08 +0530

    Added Organization

--- app/(organizationCreation)/organization/page.tsx
+++ app/(organizationCreation)/organization/page.tsx
@@ -1,11 +1,44 @@
-import CreateOrganisation from "@/components/createOrg/create-organisation"
+import FormCreateOrg from "@/components/organization/form-create-org"
+import CreateOrganisation from "@/components/organization/form-create-org"
+import HeroHeader from "@/components/hero/heroheader"
+import { Button } from "@/components/ui/button";
+import {
+    SignInButton,
+    UserButton,
+    currentUser
+} from "@clerk/nextjs";
+import OrganisationList from "@/components/organization/organization-list";
 
+const OrganisationRegs = async () => {
 
-const OrganisationRegs=()=>{
-  
-    return(
+    const user = await currentUser();
+    console.log("User: ")
+    console.log(user);
+
+    return (
         <>
-   <CreateOrganisation/>
+            {!user && (
+                <div className="container h-screen flex flex-col gap-3 items-center justify-center">
+                    <h1 className="text-xl font-semibold text-center">
+                        You are not logges in please login to continue
+                    </h1>
+                    <SignInButton>
+                        <Button size="sm" variant="primary">
+                            Login
+                        </Button>
+                    </SignInButton>
+                </div>
+            )}
+
+            {!!user && (
+                <main className="bg-slate-950">
+                    <HeroHeader />
+                    <div className="container max-w-screen-2xl mx-auto ">
+                        <OrganisationList />
+                        <FormCreateOrg />
+                    </div>
+                </main>
+            )}
         </>
     )
 }
