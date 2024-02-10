e7ede37e18a9a5aed3c3c82413d2b647f3bc5604
Ayush-Bulbule <ayushbulbule24@gmail.com>
2024-02-11 00:49:56 +0530

    merge from pranay

---
+++ app/(organizationCreation)/organization/page.tsx
@@ -0,0 +1,13 @@
+import CreateOrganisation from "@/components/createOrg/create-organisation"
+
+
+const OrganisationRegs=()=>{
+  
+    return(
+        <>
+   <CreateOrganisation/>
+        </>
+    )
+}
+
+export default OrganisationRegs
